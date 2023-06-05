dc90a24d509d580caeac18c195ba4a662cf9a160
badmood111 <petrzela.adam@seznam.cz>
2023-06-05 19:51:11 +0200

    setup content

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,25 @@
   import "./app.css";
 </script>
 
-<h1 class="bg-blue-500">Hello!</h1>
+<main class="h-[100vh] w-[100vw] flex flex-col mx-auto">
+  <img
+    src="./images/illustration-sign-up-mobile.svg"
+    alt="sign up illustration"
+  />
+
+  <h1>Stay updated!</h1>
+  <p>Join 60,000+ product managers receiving monthly updates on:</p>
+  <ul>
+    <li>Product discovery and building what matters</li>
+    <li>Measuring to ensure updates are a success</li>
+    <li>And much more!</li>
+  </ul>
+
+  <form action="">
+    <label>
+      <p class="">Email address</p>
+      <input class="block" type="text" placeholder="email@company.com" />
+      <button class="block">Subscribe to monthly newsletter</button>
+    </label>
+  </form>
+</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "tomato": "hsl(4, 100%, 67%)",
+        "darkSlate" : "hsl(234, 29%, 20%)",
+        "charcoal": "hsl(235, 18%, 26%)",
+        "grey": "hsl(231, 7%, 60%)",
+      }
+    },
   },
   plugins: [],
 };
