77fec9280041b4606775837c6c707546c7cd6d00
badmood111 <petrzela.adam@seznam.cz>
2023-06-06 20:28:53 +0200

    mobile design finished

--- src/App.svelte
+++ src/App.svelte
@@ -8,9 +8,9 @@
 <svelte:window bind:innerWidth={width} />
 
 <main
-  class="mx-auto flex h-[100vh] w-[100vw] flex-col font-roboto text-darkSlate"
+  class="mx-auto flex h-[100vh] w-[100vw] flex-col font-roboto text-charcoal"
 >
-  {#if width <= 375}
+  {#if width <= 500}
     <img
       src="./images/illustration-sign-up-mobile.svg"
       alt="sign up illustration"
@@ -21,12 +21,12 @@
       alt="sign up illustration"
     />
   {/if}
-  <div class="mx-auto mt-10">
-    <h1 class="text-6xl font-bold">Stay updated!</h1>
+  <div class="mx-auto mt-10 p-6">
+    <h1 class="text-5xl font-bold">Stay updated!</h1>
     <p class="mt-7 text-xl">
       Join 60,000+ product managers receiving monthly updates on:
     </p>
-    <ul class="mt-7">
+    <ul class="mt-7 flex flex-col gap-4">
       <IconLi>Product discovery and building what matters</IconLi>
       <IconLi>Measuring to ensure updates are a success</IconLi>
       <IconLi>And much more!</IconLi>
--- src/components/IconLi.svelte
+++ src/components/IconLi.svelte
@@ -1,4 +1,4 @@
 <li class="flex gap-5 text-xl">
-  <img class="inline" src="./images/icon-list.svg" alt="icon">
+  <img class="inline" src="./images/icon-list.svg" alt="icon" />
   <slot />
 </li>
